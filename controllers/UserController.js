b9d818898b0af3b023c3914c4bb501b11eb5ed65
Jonas <mz-martinez@live.com>
2022-04-27 20:34:20 -0300

    Adicionado ID e separado por particularidades

---
+++ controllers/UserController.js
@@ -0,0 +1,12 @@
+class UserController{
+
+   constructor(valorInicial, valorMensal, valorJuros, valorPeriodo){
+       
+    this.valorInicial = document.getElementById(valorInicial)
+    this.valorMensal = document.getElementById(valorMensal)
+    this.valorJuros = document.getElementById(valorJuros)
+    this.valorPeriodo = document.getElementById(valorPeriodo)
+
+   }
+
+}
---
+++ models/usuario.js
@@ -0,0 +1,12 @@
+class User{
+
+    constructor(inicial, mensal, jurosAnual, periodo){
+
+        this.inicial = inicial;
+        this.mensal = mensal;
+        this.jurosAnual = jurosAnual;
+        this.periodo = periodo;
+
+    }
+
+}
